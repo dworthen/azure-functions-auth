6139c1547eba07635074d5b612458c37982b7132
Worthen <dworthen@wsu.edu>
2018-10-17 11:00:34 -0700

    call azure function

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
-var Component = {
+var User = {
   view: function(vnode) {
-    return m("main", [m("h1", "Azure Auth Funtions with mithril")]);
+    return m("li", vnode.attrs.name);
   }
 };
 
-m.mount(document.getElementById("app"), Component);
+var Users = {
+  view: function(vnode) {
+    return m(
+      "ul",
+      (vnode.attrs.users || []).map(function(user) {
+        return m(User, { name: user.Name });
+      })
+    );
+  }
+};
+
+var Home = {
+  users: [],
+  oninit: function(vnode) {
+    m.request({
+      method: "GET",
+      url: "https://dworthentestfunctionapp1.azurewebsites.net/api/users"
+    }).then(function(results) {
+      vnode.state.users = results;
+    });
+  },
+  view: function(vnode) {
+    return m("main", [
+      m("h1", "Azure Auth Funtions with mithril"),
+      m(Users, { users: vnode.state.users })
+    ]);
+  }
+};
+
+m.mount(document.getElementById("app"), Home);
